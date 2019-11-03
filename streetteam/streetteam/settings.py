d4c6a5dfa9296998600e6ae2f347ce3cf420a285
Aly Sivji <alysivji@gmail.com>
2019-11-03 08:54:02 -0600

    update database settings to use env vars:

--- streetteam/streetteam/settings.py
+++ streetteam/streetteam/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'streetteam.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'streetteam',
-        'USER': 'streetteam_user',
-        'PASSWORD': 'streetteam_password',
-        'HOST': 'db',
-        'PORT': '5432',
+        'NAME': os.getenv("POSTGRES_DB"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'HOST': os.getenv("POSTGRES_HOST"),
+        'PORT': os.getenv("POSTGRES_PORT"),
     }
 }
 
