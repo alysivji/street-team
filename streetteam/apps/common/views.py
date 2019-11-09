65526f312ee2451fdc998af273b4a4e0f972d1b9
Aly Sivji <alysivji@gmail.com>
2019-11-09 13:50:07 -0600

    change to test -- will revert

--- streetteam/apps/common/views.py
+++ streetteam/apps/common/views.py
@@ -1,5 +1,10 @@
+from django.conf import settings
+from django.utils.decorators import method_decorator
+
 from rest_framework.views import APIView
 from django.http import HttpResponse
+
+from apps.twilio_integration.decorators import validate_twilio_request
 
 
 class DebugEndpoint(APIView):
@@ -7,7 +12,7 @@ class DebugEndpoint(APIView):
         print(request.build_absolute_uri())
         return HttpResponse()
 
-    # @method_decorator(validate_twilio_request(settings.TWILIO_AUTH_TOKEN))
+    @method_decorator(validate_twilio_request(settings.TWILIO_AUTH_TOKEN))
     def post(self, request, format=None):
         print(request.build_absolute_uri())
         print(request.META)
