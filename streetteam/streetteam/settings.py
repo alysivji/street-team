adcfca0457ec8aa3e48ad7fdaef08283b1931043
Aly Sivji <alysivji@gmail.com>
2019-11-09 13:47:59 -0600

    add header setting to django

--- streetteam/streetteam/settings.py
+++ streetteam/streetteam/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if IN_PRODUCTION else True
 
-# Development environment setting
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')  # set in traefik
 ALLOWED_HOSTS = ["0.0.0.0", ".sivji.com"] if IN_PRODUCTION else ["0.0.0.0", ".ngrok.io"]
 APPEND_SLASH = True
 
