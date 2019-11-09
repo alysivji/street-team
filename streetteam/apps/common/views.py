320f4b826fcdbab877fa08f13e970e3232874a22
Aly Sivji <alysivji@gmail.com>
2019-11-09 13:02:33 -0600

    print requests

--- streetteam/apps/common/views.py
+++ streetteam/apps/common/views.py
@@ -10,4 +10,5 @@ class DebugEndpoint(APIView):
     # @method_decorator(validate_twilio_request(settings.TWILIO_AUTH_TOKEN))
     def post(self, request, format=None):
         print(request.build_absolute_uri())
+        print(request.META)
         return HttpResponse()
