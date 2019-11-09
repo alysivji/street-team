520e4754f1e6cf8ba11ea1d68a513ae921f8d691
Aly Sivji <alysivji@gmail.com>
2019-11-09 12:12:40 -0600

    add debug endpoint

---
+++ streetteam/apps/common/views.py
@@ -0,0 +1,13 @@
+from rest_framework.views import APIView
+from django.http import HttpResponse
+
+
+class DebugEndpoint(APIView):
+    def get(self, request, format=None):
+        print(request.build_absolute_uri())
+        return HttpResponse()
+
+    # @method_decorator(validate_twilio_request(settings.TWILIO_AUTH_TOKEN))
+    def post(self, request, format=None):
+        print(request.build_absolute_uri())
+        return HttpResponse()
