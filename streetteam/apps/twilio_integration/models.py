654751854dd4157a1c7bc1e0544c8a022fe06bb2
Aly Sivji <4369343+alysivji@users.noreply.github.com>
2019-11-11 08:48:13 -0600

    Add created_at and updated_at fields to models (#16)

--- streetteam/apps/twilio_integration/models.py
+++ streetteam/apps/twilio_integration/models.py
@@ -1,13 +1,14 @@
 from django.contrib.postgres.fields import JSONField
 from django.db import models
+from apps.common.models import BaseModel
 
 
-class PhoneNumber(models.Model):
+class PhoneNumber(BaseModel):
     id = models.AutoField(primary_key=True)
     number = models.CharField(max_length=30, unique=True)
 
 
-class ReceivedMessage(models.Model):
+class ReceivedMessage(BaseModel):
     id = models.AutoField(primary_key=True)
     twilio_message_id = models.CharField(max_length=34)
     data = JSONField()
