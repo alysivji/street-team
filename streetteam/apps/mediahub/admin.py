2fcf4a1963319f0dcd3759f226c2e3161dd20b2b
Aly Sivji <alysivji@gmail.com>
2019-11-17 01:17:00 -0600

    update change view for admin to show picture

--- streetteam/apps/mediahub/admin.py
+++ streetteam/apps/mediahub/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from .models import MediaResource
 
 
+@admin.register(MediaResource)
 class MediaResourceAdmin(admin.ModelAdmin):
-    pass
+    change_form_template = "admin/media_resource_preview.html"
 
-
-admin.site.register(MediaResource, MediaResourceAdmin)
+    def change_view(self, request, object_id, form_url="", extra_context=None):
+        extra_context = extra_context or {}
+        extra_context["image_url"] = MediaResource.objects.get(pk=object_id).resource_url
+        return super().change_view(request, object_id, form_url, extra_context=extra_context)
