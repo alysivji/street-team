708cc396e0df18f0fc2154d235e8b68a8f4c7018
Aly Sivji <alysivji@gmail.com>
2019-11-18 22:59:11 -0600

    remove useless check

--- streetteam/apps/users/managers.py
+++ streetteam/apps/users/managers.py
@@ -12,8 +12,6 @@ class UserManager(BaseUserManager):
         """
         Create and save a User with the given email and password.
         """
-        if not email:
-            raise ValueError(_("The Email must be set"))
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         if password:
