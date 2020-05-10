772985645ddfecc68e74a30f77e1cb385ebe7938
Aly Sivji <alysivji@gmail.com>
2020-05-10 18:10:20 -0500

    get crop working

--- streetteam/apps/mediahub/static/mediahub/crop.js
+++
@@ -1,9 +0,0 @@
-import 'cropperjs/dist/cropper.css';
-import Cropper from 'cropperjs';
-
-const image = document.getElementById('image');
-const cropper = new Cropper(image, {
-  viewMode: 1,
-  dragMode: 'crop',
-  initialAspectRatio: 16 / 9
-});
