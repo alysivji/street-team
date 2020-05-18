27a5949fbaef41772fadbc5ed179a3fd67819273
Aly Sivji <alysivji@gmail.com>
2020-05-17 19:44:29 -0500

    add fk relationship to teams

--- streetteam/apps/events/models.py
+++ streetteam/apps/events/models.py
@@ -1,5 +1,9 @@
 import logging
+import uuid
+
 from django.db import models
+
+from apps.teams.models import Team
 from common.models import BaseModel
 
 logger = logging.getLogger(__name__)
@@ -7,12 +11,13 @@ logger = logging.getLogger(__name__)
 
 class TeamEvent(BaseModel):
     id = models.AutoField(primary_key=True)
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True, null=False)
+    team = models.ForeignKey(Team, related_name="events", on_delete=models.CASCADE, null=False)
 
     title = models.CharField(null=False, max_length=255)
     description = models.TextField(blank=True, null=False)
     happens_on = models.DateTimeField(null=False)
 
-    # TODO foreign key with teams
     # TODO state machine for event_status field
     # Active
     # Inactive
