409a687682c9915ab9375667df6c971f368faf19
Aly Sivji <alysivji@gmail.com>
2020-05-21 22:01:54 -0500

    user cannot join team

--- streetteam/apps/teams/tests/functional_test.py
+++ streetteam/apps/teams/tests/functional_test.py
@@ -9,14 +9,13 @@ from apps.users.tests.factories import UserFactory
 @pytest.mark.django_db
 @pytest.mark.end2end
 class TestUserJoiningTeamWorkflow:
-    @pytest.mark.current
     def test_user_joins_team__happy_path(self, client, login_user):
         # Arrange
         team = TeamFactory()
         user = UserFactory()
         login_user(user)
 
-        # user enters join code successfully; use the view
+        # Act
         resp = client.post(f"/teams/join/", {"join_code": team.join_code})
 
         # check that user is on team
@@ -24,3 +23,21 @@ class TestUserJoiningTeamWorkflow:
         conditions = {"team": team, "user": user}
         membership = UserTeamMembership.objects.filter(**conditions).all()
         assert len(membership) == 1
+
+    @pytest.mark.current
+    def test_user_joins_team_with_old_join_code__rejected(self, client, login_user):
+        # Arrange
+        team = TeamFactory()
+        old_join_code = team.join_code
+        team.generate_new_join_code()
+        user = UserFactory()
+        login_user(user)
+
+        # Act
+        resp = client.post(f"/teams/join/", {"join_code": old_join_code})
+
+        # Assert user should not be on team
+        assert resp.status_code == 200
+        conditions = {"team": team, "user": user}
+        membership = UserTeamMembership.objects.filter(**conditions).all()
+        assert len(membership) == 0
