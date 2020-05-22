71dbee08a47d19ea78f735c640b07f9a7af921a8
Aly Sivji <alysivji@gmail.com>
2020-05-22 18:47:46 -0500

    update state, it's easier

--- streetteam/apps/events/interactors.py
+++ streetteam/apps/events/interactors.py
@@ -4,8 +4,5 @@ from .models import Event
 def create_event(user, team, event_information):
     event = Event(team=team, **event_information)
     event.save()
-
-    # TODO do not save full event first time
-    # or just save it again to capture in log
-    # event.log.append({"action": "create_event", "user_id": user.id})
-    # event.save()
+    event.update_draft(by=user)
+    event.save()
