03a4105454b70844818516888d72b6f98e48f57d
Aly Sivji <alysivji@gmail.com>
2020-05-22 15:04:14 -0500

    add state machine transitions with condition tests

--- streetteam/apps/events/models.py
+++ streetteam/apps/events/models.py
@@ -1,13 +1,26 @@
+from datetime import datetime, timezone
 import logging
 import uuid
 
-from django_fsm import FSMField
+from django_fsm import FSMField, transition
+from django_fsm_log.decorators import fsm_log_by
 from django.db import models
 
 from apps.teams.models import Team
 from common.models import BaseModel
 
 logger = logging.getLogger(__name__)
+
+
+def is_future_event(instance):
+    now = datetime.now(timezone.utc)
+    if instance.happens_on > now:
+        return True
+    return False
+
+
+def is_past_event(instance):
+    return not is_future_event(instance)
 
 
 class Event(BaseModel):
@@ -37,3 +50,41 @@ class Event(BaseModel):
         INITIAL_STATE = DRAFT
 
     event_status = FSMField(default=EventStatus.INITIAL_STATE, choices=EventStatus.CHOICES)
+
+    @fsm_log_by
+    @transition(field=event_status, source=EventStatus.DRAFT, target=EventStatus.DRAFT)
+    def update_draft(self, by):
+        pass
+
+    @fsm_log_by
+    @transition(field=event_status, source=EventStatus.DRAFT, target=EventStatus.ACTIVE, conditions=[is_future_event])
+    def publish(self, by):
+        pass
+
+    @fsm_log_by
+    @transition(field=event_status, source=EventStatus.ACTIVE, target=EventStatus.INACTIVE)
+    def hide(self, by):
+        pass
+
+    @fsm_log_by
+    @transition(
+        field=event_status, source=EventStatus.INACTIVE, target=EventStatus.ACTIVE, conditions=[is_future_event]
+    )
+    def unhide(self, by):
+        pass
+
+    @fsm_log_by
+    @transition(
+        field=event_status,
+        source=[EventStatus.DRAFT, EventStatus.ACTIVE, EventStatus.INACTIVE],
+        target=EventStatus.DELETED,
+        conditions=[is_future_event],
+    )
+    def delete(self, by):
+        pass
+
+    @fsm_log_by
+    @transition(field=event_status, source=EventStatus.ACTIVE, target=EventStatus.COMPLETED, conditions=[is_past_event])
+    def archive(self, by):
+        """Should this be a user action or a CRON job if the event is over a few days old"""
+        pass
--- streetteam/apps/events/tests/models_test.py
+++ streetteam/apps/events/tests/models_test.py
@@ -1,6 +1,7 @@
+from datetime import datetime, timedelta, timezone
 import pytest
 
-from ..models import Event
+from ..models import Event, is_future_event, is_past_event
 from .factories import TeamEventFactory
 
 
@@ -14,3 +15,38 @@ class TestEventModel:
         assert record.title == event.title
         assert record.description == event.description
         assert record.event_status == Event.EventStatus.DRAFT
+
+
+@pytest.mark.django_db
+class TestEventStatustateMachine:
+    def test_is_future_event__event_happens_tomorrow(self):
+        tomorrow = datetime.now(timezone.utc) + timedelta(days=1)
+        event = TeamEventFactory(happens_on=tomorrow)
+        assert is_future_event(event) is True
+
+    @pytest.mark.freeze_time
+    def test_is_future_event__event_happens_now(self, freezer):
+        now = datetime.now(timezone.utc)
+        event = TeamEventFactory(happens_on=now)
+        assert is_future_event(event) is False
+
+    def test_is_future_event__event_happened_yesterday(self):
+        yesterday = datetime.now(timezone.utc) - timedelta(days=1)
+        event = TeamEventFactory(happens_on=yesterday)
+        assert is_future_event(event) is False
+
+    def test_is_past_event__event_happens_tomorrow(self):
+        tomorrow = datetime.now(timezone.utc) + timedelta(days=1)
+        event = TeamEventFactory(happens_on=tomorrow)
+        assert is_past_event(event) is False
+
+    @pytest.mark.freeze_time
+    def test_is_past_event__event_happens_now(self, freezer):
+        now = datetime.now(timezone.utc)
+        event = TeamEventFactory(happens_on=now)
+        assert is_past_event(event) is True
+
+    def test_is_past_event__event_happened_yesterday(self):
+        yesterday = datetime.now(timezone.utc) - timedelta(days=1)
+        event = TeamEventFactory(happens_on=yesterday)
+        assert is_past_event(event) is True
