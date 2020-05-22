64a5e8642b245981cf92e3ffc4cafa01c4854427
Aly Sivji <alysivji@gmail.com>
2020-05-22 14:52:54 -0500

    move to class based tests

--- streetteam/apps/events/tests/models_test.py
+++ streetteam/apps/events/tests/models_test.py
@@ -5,11 +5,12 @@ from .factories import TeamEventFactory
 
 
 @pytest.mark.django_db
-def test_create_and_retrieve_event():
-    event = TeamEventFactory(title="My event", description="Gonna be off the chain")
+class TestEventModel:
+    def test_create_and_retrieve_event(self):
+        event = TeamEventFactory(title="My event", description="Gonna be off the chain")
 
-    record = Event.objects.first()
+        record = Event.objects.first()
 
-    assert record.title == event.title
-    assert record.description == event.description
-    assert record.event_status == Event.EventStatus.DRAFT
+        assert record.title == event.title
+        assert record.description == event.description
+        assert record.event_status == Event.EventStatus.DRAFT
