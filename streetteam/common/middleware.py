5525a3c82e9242af23ccf03a37fc02e92cae5fff
Aly Sivji <alysivji@gmail.com>
2020-05-23 15:35:37 -0500

    replace request uuid with header

--- streetteam/common/middleware.py
+++ streetteam/common/middleware.py
@@ -29,7 +29,10 @@ class RequestUuidMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        request.request_uuid = str(uuid.uuid4())
+        request_id = request.headers.get("X-Request-ID")
+        if not request_id:
+            request_id = str(uuid.uuid4())
+        request.request_id = request_id
         response = self.get_response(request)
         return response
 
