98edb7c0ae8f86f4169a5d47f825e4ab15bba7ab
Aly Sivji <alysivji@gmail.com>
2020-05-23 15:16:02 -0500

    test for attribute error

--- streetteam/common/tests/test_middleware.py
+++ streetteam/common/tests/test_middleware.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 class TestRequestUuidMiddleware:
     def test_request_has_uuid(self, client):
         response = client.get("/healthcheck")
@@ -7,4 +10,5 @@ class TestRequestUuidMiddleware:
     def test_request_does_not_have_guid_field(self, client):
         response = client.get("/healthcheck")
         request = response.wsgi_request
-        assert getattr(request, "request_guid") is None
+        with pytest.raises(AttributeError):
+            getattr(request, "request_guid")
