43183fda017c67409617be65044c68e767566323
Aly Sivji <alysivji@gmail.com>
2022-03-21 10:46:00 -0500

    clean up linebreak

--- streetteam/apps/users/migrations/0001_rebuild_user_model_from_scratch.py
+++ streetteam/apps/users/migrations/0001_rebuild_user_model_from_scratch.py
@@ -36,7 +36,10 @@ class Migration(migrations.Migration):
                     "groups",
                     models.ManyToManyField(
                         blank=True,
-                        help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",  # noqa
+                        help_text=(
+                            "The groups this user belongs to. A user will get all permissions "
+                            "granted to each of their groups."
+                        ),
                         related_name="user_set",
                         related_query_name="user",
                         to="auth.Group",
