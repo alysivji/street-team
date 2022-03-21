a368f6bbb7d35699fd0eb1d64173103903c48ef6
Aly Sivji <alysivji@gmail.com>
2022-03-21 14:18:35 -0500

    get jwt login working

---
+++ streetteam/apps/users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from . import views
+
+urlpatterns = [
+    path("/", views.ContactCreateAPI.as_view()),
+    path("/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
